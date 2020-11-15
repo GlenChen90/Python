46fe77d3832575d670151c2beeff9d9685aa6722
Glen Chen <329587790@qq.com>
2020-11-15 17:28:21 +0800

    first plane commit

---
+++ plane/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='plane',
+    version='1.00',
+    packages=[''],
+    url='',
+    license='',
+    author='Glen',
+    author_email='',
+    description='plane'
+)
